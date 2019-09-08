ef834b56cc4220206a456aae4bd5e1e48df543bd
nokchax <nokchax@gmail.com>
2019-09-08 15:18:18 +0900

    feat : add external file properties

--- src/main/java/com/zum/escape/api/ApiApplication.java
+++ src/main/java/com/zum/escape/api/ApiApplication.java
@@ -1,13 +1,19 @@
 package com.zum.escape.api;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        ApiContextInitializer.init();
+
+        new SpringApplicationBuilder()
+                .sources(ApiApplication.class)
+                .properties("spring.config.location=file:/data/etc/escape/application.yml")
+                .run(args);
     }
 
 }
