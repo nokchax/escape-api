3e77c06c35580c93b5c0d7ff67ebde9af5e6b61c
nokchax <nokchax@gmail.com>
2019-09-08 16:07:42 +0900

    feat : leetcode page crawling test

--- src/test/java/com/zum/escape/api/thirdPartyAdapter/leetcode/service/LeetCodeServiceTest.java
+++ src/test/java/com/zum/escape/api/thirdPartyAdapter/leetcode/service/LeetCodeServiceTest.java
@@ -2,6 +2,11 @@ package com.zum.escape.api.thirdPartyAdapter.leetcode.service;
 
 
 import com.zum.escape.api.thirdPartyAdapter.leetcode.response.ProblemResponse;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.*;
@@ -9,9 +14,11 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.Collections;
 
 public class LeetCodeServiceTest {
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36";
     private RestTemplate restTemplate = new RestTemplate();
 
     @Before
@@ -38,7 +45,36 @@ public class LeetCodeServiceTest {
 
     private HttpHeaders makeHttpHeaders() {
         HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36");
+        headers.set(HttpHeaders.USER_AGENT, USER_AGENT);
         return headers;
+    }
+
+
+    @Test
+    public void jsoupTest() throws IOException {
+        Connection connection = Jsoup.connect("https://leetcode.com/" + "nokchax");
+        connection.header(HttpHeaders.USER_AGENT, USER_AGENT);
+
+
+        Document document = connection.get();
+        System.out.println(document);
+
+        Elements elements = document.getElementsByClass("progress-bar-success");
+        Elements elements1 = document.getElementsByTag("a");
+        System.out.println("=============================================================");
+        System.out.println(elements.get(1).text());
+        System.out.println("=============================================================");
+        System.out.println(elements1);
+        Elements href = document.getElementsByAttributeValueStarting("href", "/problems");
+        System.out.println("=============================================================");
+        System.out.println(href);
+        System.out.println("=============================================================");
+        for(Element e : href) {
+            String problemr1 = e.attr("href");
+            String problem = e.getElementsByTag("b").text();
+            String accepted= e.getElementsByTag("span").get(0).text();
+            System.out.println(problem + " : " + accepted);
+        }
+
     }
 }
