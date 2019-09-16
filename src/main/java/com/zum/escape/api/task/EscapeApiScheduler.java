29a5168f0ee1cecb417df7ad9ffddce1142f85e9
nokchax <nokchax@gmail.com>
2019-09-16 18:51:59 +0900

    feat : change scheduler bean name (duplicated with spring taskScheduler)

--- src/main/java/com/zum/escape/api/task/EscapeApiScheduler.java
+++ src/main/java/com/zum/escape/api/task/EscapeApiScheduler.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class TaskScheduler {
+public class EscapeApiScheduler {
     private final TaskService taskService;
     private final UserHistoryService userHistoryService;
 
