c5e359a7e7709ac1375a0a88398a10fc2b2637de
nokchax <nokchax@gmail.com>
2019-10-08 20:53:15 +0900

    feat : add comments

--- src/main/java/com/zum/escape/api/task/EscapeApiScheduler.java
+++ src/main/java/com/zum/escape/api/task/EscapeApiScheduler.java
@@ -14,16 +14,19 @@ public class EscapeApiScheduler {
 
     @Scheduled(cron = "0 1 0 * * MON")
     public void createTask() {
+        // make weekly task
         taskService.createTasks();
     }
 
     @Scheduled(cron = "30 1 0 * * MON")
     public void updatePoints() {
+        // give points to all users when month start
         userHistoryService.imposeFines();
     }
 
     @Scheduled(cron = "0 0 0,9,12,15,18,21 * * *")
     public void update() {
+        // update user's status
         taskService.update();
     }
 }
