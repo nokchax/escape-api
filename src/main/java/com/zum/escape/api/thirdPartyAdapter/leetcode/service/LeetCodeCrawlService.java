e71bd12765bcd25ade2680e9612ce4c50028c428
nokchax <nokchax@gmail.com>
2019-12-03 20:34:12 +0900

    feat : make base of LeetCodeCrawlService using selenium

---
+++ src/main/java/com/zum/escape/api/thirdPartyAdapter/leetcode/service/LeetCodeCrawlService.java
@@ -0,0 +1,53 @@
+package com.zum.escape.api.thirdPartyAdapter.leetcode.service;
+
+import com.zum.escape.api.users.domain.User;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.annotation.PostConstruct;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
+public class LeetCodeCrawlService {
+    private FirefoxOptions options;
+
+    @PostConstruct
+    public void init() {
+        ClassPathResource resource = new ClassPathResource("/src/main/resources/webdriver/geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", resource.getPath());
+
+        options = new FirefoxOptions();
+        options.setHeadless(true);
+    }
+
+    public void crawl(User user) {
+        WebDriver browser = new FirefoxDriver(options);
+        WebDriverWait wait = new WebDriverWait(browser, 20);
+        browser.get("https://leetcode.com/accounts/login/");
+
+
+        WebElement button = wait.until(presenceOfElementLocated(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div/div/button")));
+
+        WebElement id = browser.findElement(By.name("login"));
+        WebElement password = browser.findElement(By.name("password"));
+
+        id.clear();
+        id.sendKeys(user.getId());
+        password.clear();
+        password.sendKeys(user.getPassword());
+        button.click();
+
+        wait.until(presenceOfElementLocated(By.xpath("//*[@id=\"nav-user-app\"]")));
+        browser.navigate().to("https://leetcode.com/api/problems/all/");
+        WebElement body = browser.findElement(By.tagName("body"));
+
+        System.out.println(body.getText());
+
+        browser.close();
+    }
+}
