892c03bd1aeb00732a71a553171eca6ee8f43ee8
nokchax <nokchax@gmail.com>
2019-12-06 14:42:13 +0900

    refactor : refactor crawl method easy to read

--- src/main/java/com/zum/escape/api/thirdPartyAdapter/leetcode/service/LeetCodeCrawlService.java
+++ src/main/java/com/zum/escape/api/thirdPartyAdapter/leetcode/service/LeetCodeCrawlService.java
@@ -15,6 +15,7 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElemen
 
 public class LeetCodeCrawlService {
     private static final String LEETCODE_LOGIN_URL = "https://leetcode.com/accounts/login/";
+    private static final String LEETCODE_LOGOUT_URL = "https://leetcode.com/accounts/logout/";
     private static final String LEETCODE_API_URL = "https://leetcode.com/api/problems/all/";
     private static final String XPATH_OF_LOGIN_BUTTON = "/html/body/div[1]/div/div[2]/div/div[2]/div/div/div/button";
     private static final String XPATH_OF_NAV_BUTTON = "//*[@id=\"nav-user-app\"]";
@@ -29,14 +30,29 @@ public class LeetCodeCrawlService {
         options.setHeadless(true);
     }
 
+    // TODO: 2019-12-06 make thread pool of browser or make login logic serial (this can be very slow)
     public void crawl(User user) {
         WebDriver browser = new FirefoxDriver(options);
         WebDriverWait wait = new WebDriverWait(browser, 20);
         browser.get(LEETCODE_LOGIN_URL);
 
+        login(user, browser, wait);
 
+        String jsonBody = callProblemApi(browser, wait);
+
+        logout(browser);
+    }
+
+    private String callProblemApi(WebDriver browser, WebDriverWait wait) {
+        wait.until(presenceOfElementLocated(By.xpath(XPATH_OF_NAV_BUTTON)));
+        browser.navigate().to(LEETCODE_API_URL);
+        WebElement body = browser.findElement(By.tagName("body"));
+
+        return body.getText();
+    }
+
+    private void login(User user, WebDriver browser, WebDriverWait wait) {
         WebElement button = wait.until(presenceOfElementLocated(By.xpath(XPATH_OF_LOGIN_BUTTON)));
-
         WebElement id = browser.findElement(By.name("login"));
         WebElement password = browser.findElement(By.name("password"));
 
@@ -45,14 +61,11 @@ public class LeetCodeCrawlService {
         password.clear();
         password.sendKeys(user.getPassword());
         button.click();
+    }
 
-        wait.until(presenceOfElementLocated(By.xpath(XPATH_OF_NAV_BUTTON)));
-        browser.navigate().to(LEETCODE_API_URL);
-        WebElement body = browser.findElement(By.tagName("body"));
 
-        System.out.println(body.getText());
-
-        // TODO: 2019/12/03 do not close browser and reuse it
-        browser.close();
+    private void logout(WebDriver browser) {
+        browser.navigate()
+                .to(LEETCODE_LOGOUT_URL);
     }
 }
