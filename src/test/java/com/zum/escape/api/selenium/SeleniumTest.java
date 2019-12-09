655dd88616804b6a6a611f466fe1dae773a7d503
nokchax <nokchax@gmail.com>
2019-12-09 21:58:54 +0900

    feat : tearful efforts to escape recaptcha ㅠㅠ

--- src/test/java/com/zum/escape/api/selenium/SeleniumTest.java
+++ src/test/java/com/zum/escape/api/selenium/SeleniumTest.java
@@ -1,49 +1,72 @@
 package com.zum.escape.api.selenium;
 
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zum.escape.api.thirdPartyAdapter.leetcode.response.ProblemResponse;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
-@Slf4j
+//@Slf4j
 public class SeleniumTest {
     @Test
-    public void seleniumTest() {
-        ClassPathResource resource = new ClassPathResource("/src/main/resources/webdriver/geckodriver.exe");
+    public void seleniumTest() throws InterruptedException, IOException {
+        ClassPathResource resource = new ClassPathResource("/src/main/resources/webdriver/geckodriver(mac)");
         System.setProperty("webdriver.gecko.driver", resource.getPath());
 
         FirefoxOptions options = new FirefoxOptions();
-        options.setHeadless(true);
+//        options.setHeadless(true);
+        // change user agent using randomization or round robin each request
+        options.merge(DesiredCapabilities.operaBlink());
         WebDriver browser = new FirefoxDriver(options);
         WebDriverWait wait = new WebDriverWait(browser, 20);
         browser.get("https://leetcode.com/accounts/login/");
 
 
-        WebElement button = wait.until(presenceOfElementLocated(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div/div/button")));
-
         WebElement id = browser.findElement(By.name("login"));
         WebElement password = browser.findElement(By.name("password"));
+        Thread.sleep(2000);
 
-        id.clear();
-        id.sendKeys("nokchax");
+        id.click();
+        humanize(id, "nokchax");
+        Thread.sleep(300);
+        id.sendKeys(Keys.TAB);
         password.clear();
-        password.sendKeys("");
-        button.click();
+        humanize(password, "");
+        Thread.sleep(300);
+        password.sendKeys(Keys.RETURN);
+//        button.click();
 
         wait.until(presenceOfElementLocated(By.xpath("//*[@id=\"nav-user-app\"]")));
         browser.navigate().to("https://leetcode.com/api/problems/all/");
         WebElement body = browser.findElement(By.tagName("body"));
 
-        log.info(body.getText());
+//        log.info(body.getText());
+        System.out.println(body.getText());
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ProblemResponse problemResponse = objectMapper.readValue(body.getText(), ProblemResponse.class);
+        System.out.println(problemResponse);
 
         browser.close();
+    }
+
+    private void humanize(WebElement element, String input) throws InterruptedException {
+        for(char c : input.toCharArray()) {
+            element.sendKeys(String.valueOf(c));
+            Thread.sleep(ThreadLocalRandom.current().nextLong(100) + 100);
+        }
     }
 
 }
