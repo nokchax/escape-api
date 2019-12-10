bc2d6895150eeb415d44ded63e8506b7b5b68550
nokchax <nokchax@gmail.com>
2019-12-10 18:33:20 +0900

    feat : add user agent queue (thread safe)

---
+++ src/main/java/com/zum/escape/api/users/service/UserAgentQueue.java
@@ -0,0 +1,28 @@
+package com.zum.escape.api.users.service;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+@Component
+public class UserAgentQueue {
+    private BlockingQueue<DesiredCapabilities> USER_AGENT_QUEUE = new LinkedBlockingQueue<>();
+
+    @PostConstruct
+    private void init() {
+        USER_AGENT_QUEUE.add(DesiredCapabilities.chrome());
+        USER_AGENT_QUEUE.add(DesiredCapabilities.firefox());
+        USER_AGENT_QUEUE.add(DesiredCapabilities.operaBlink());
+    }
+
+    public DesiredCapabilities peek() {
+        DesiredCapabilities frontUserAgent = USER_AGENT_QUEUE.poll();
+
+        USER_AGENT_QUEUE.add(frontUserAgent);
+
+        return frontUserAgent;
+    }
+}
