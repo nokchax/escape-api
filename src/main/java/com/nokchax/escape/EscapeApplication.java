4c8f7f0243833bb034bae593233cee26bdebe574
nokchax <nokchax@gmail.com>
2020-02-04 20:08:18 +0900

    feat : remove previous start point

--- src/main/java/com/nokchax/escape/EscapeApplication.java
+++ src/main/java/com/nokchax/escape/EscapeApplication.java
@@ -4,15 +4,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.telegram.telegrambots.ApiContextInitializer;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class EscapeApplication {
     public static void main(String[] args) {
-//        // to init telegram bot
-//        ApiContextInitializer.init();
-//
-//        new SpringApplicationBuilder()
-//                .sources(EscapeApplication.class)
-//                .properties("spring.config.additional-location=file:/data/etc/escape/token.yml")
-//                .run(args);
+        // to init telegram bot
+        ApiContextInitializer.init();
+
+        new SpringApplicationBuilder()
+                .sources(EscapeApplication.class)
+                .properties("spring.config.additional-location=file:/data/etc/escape/token.yml")
+                .run(args);
     }
 }
