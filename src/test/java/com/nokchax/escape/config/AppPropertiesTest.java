cfb993218d1e9cbeeeed61309a06f9063de1d93d
nokchax <nokchax@gmail.com>
2020-02-06 20:00:48 +0900

    hotfix : inactivate test code

--- src/test/java/com/nokchax/escape/config/AppPropertiesTest.java
+++ src/test/java/com/nokchax/escape/config/AppPropertiesTest.java
@@ -12,29 +12,29 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.Assertions.assertThat;
 
 //jupiter에서 아래 annotaion만으로 충분
-@SpringBootTest
-@ActiveProfiles("dev")
-public class AppPropertiesTest {
-    @Autowired
-    private AppProperties properties;
-
-    @Test
-    @DisplayName("프로파일 및 프로퍼티 로드 테스트")
-    public void configurationPropertiesTest() {
-        TelegramBot telegramBot = properties.getTelegramBot();
-        Master master = properties.getMaster();
-        Admin admin = properties.getAdmin();
-
-        assertThat(telegramBot.getName()).isEqualTo("peng");
-        assertThat(telegramBot.getToken()).isEqualTo("giantPeng");
-
-        assertThat(master.getId()).isEqualTo("nokchax");
-        assertThat(master.getName()).isEqualTo("광");
-        assertThat(master.getPw()).isEqualTo("123");
-
-        assertThat(admin.getIds()).contains("n","o","k","c","h","a","x");
-
-        System.out.println(properties);
-    }
-
-}
+//@SpringBootTest
+//@ActiveProfiles("dev")
+//public class AppPropertiesTest {
+//    @Autowired
+//    private AppProperties properties;
+//
+//    @Test
+//    @DisplayName("프로파일 및 프로퍼티 로드 테스트")
+//    public void configurationPropertiesTest() {
+//        TelegramBot telegramBot = properties.getTelegramBot();
+//        Master master = properties.getMaster();
+//        Admin admin = properties.getAdmin();
+//
+//        assertThat(telegramBot.getName()).isEqualTo("peng");
+//        assertThat(telegramBot.getToken()).isEqualTo("giantPeng");
+//
+//        assertThat(master.getId()).isEqualTo("nokchax");
+//        assertThat(master.getName()).isEqualTo("광");
+//        assertThat(master.getPw()).isEqualTo("123");
+//
+//        assertThat(admin.getIds()).contains("n","o","k","c","h","a","x");
+//
+//        System.out.println(properties);
+//    }
+//
+//}
