8973789c30c5ef18c4775d12d9fd3824cfee5e76
nokchax <nokchax@gmail.com>
2020-02-06 20:13:34 +0900

    feat : rollback spring boot version

--- src/main/java/com/nokchax/escape/config/properties/TelegramBot.java
+++
@@ -1,9 +0,0 @@
-package com.nokchax.escape.config.properties;
-
-import lombok.Data;
-
-@Data
-public class TelegramBot {
-    private String name;
-    private String token;
-}
--- src/main/java/com/zum/escape/api/telegram/distributor/MessageDistributor.java
+++ src/main/java/com/zum/escape/api/telegram/distributor/MessageDistributor.java
@@ -43,7 +43,7 @@ public class MessageDistributor {
                         "4. 보너스(방학) 포인트 확인 : /po or /point\n" +
                         "5. 미납 벌금 조회 : /f or /fine\n" +
                         "6. 총 푼 문제 내역조회 : /h or /history username\n" +
-                        "7. 문제별 푼 사용자 리스트 : /pr or /problem 문제번호\n\n" +
+                        "7. 문제별 푼 사용자 리스트 : /pr or /problem 문제번호\n" +
                         "8. 금주 미션 참가 리스트 : /l or /list\n\n" +
                         "예) 문제를 푼다 > /update username > /todo";
 
