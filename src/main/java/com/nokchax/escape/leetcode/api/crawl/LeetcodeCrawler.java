926c80713a70e06094625e6ca1b1913df87b1681
nokchax <nokchax@gmail.com>
2020-02-13 19:39:34 +0900

    comment : add crawl type

--- src/main/java/com/nokchax/escape/leetcode/api/crawl/LeetcodeCrawler.java
+++ src/main/java/com/nokchax/escape/leetcode/api/crawl/LeetcodeCrawler.java
@@ -3,6 +3,7 @@ package com.nokchax.escape.leetcode.api.crawl;
 /*
     basic - crawl without login
     selenium - crawl with login
+    json - crawl with json file
  */
 public interface LeetcodeCrawler {
 }
