07496756be25f95484040b8f83297e069d51e9a0
nokchax <nokchax@gmail.com>
2020-02-22 22:13:42 +0900

    refactor : change user point history access modifier

--- src/main/java/com/nokchax/escape/user/domain/UserPointHistoryId.java
+++ src/main/java/com/nokchax/escape/user/domain/UserPointHistoryId.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserPointHistoryId implements Serializable {
+class UserPointHistoryId implements Serializable {
     private String user;
     private LocalDateTime dateTime;
 }
