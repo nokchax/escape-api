681b9cae0d3e5c460abb0ed9d5e39cb307e2a1af
nokchax <nokchax@gmail.com>
2020-02-29 22:06:41 +0900

    feat : add users sql test file and UserTest class

--- src/test/java/com/nokchax/escape/user/domain/UserTest.java
+++ src/test/java/com/nokchax/escape/user/domain/UserTest.java
@@ -1,9 +1,13 @@
 package com.nokchax.escape.user.domain;
 
 import com.nokchax.escape.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,4 +16,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserTest {
     @Autowired
     private UserRepository userRepository;
+
+    @Test
+    @Sql("/users.sql")
+    public void findAllUsersTest() {
+        List<User> users = userRepository.findAll();
+
+        users.forEach(System.out::println);
+    }
 }
