0a95831477a491710239ed6adbae099ea374adf6
nokchax <nokchax@gmail.com>
2020-03-10 21:13:50 +0900

    feat : illegal arguments constructor test

--- src/test/java/com/nokchax/escape/problem/domain/SolvedProblemTest.java
+++ src/test/java/com/nokchax/escape/problem/domain/SolvedProblemTest.java
@@ -5,12 +5,16 @@ import com.nokchax.escape.user.domain.User;
 import com.nokchax.escape.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 @DataJpaTest
@@ -30,9 +34,20 @@ class SolvedProblemTest {
         assertThat(user.getSolvedProblem().size()).isEqualTo(0);
     }
 
-//    @ParameterizedTest
-//    @MethodSource
-//    void testConstructorTest(String id, String password, String name) {
-//
-//    }
+    @ParameterizedTest
+    @MethodSource
+    void illegalConstructorTest(String id, String password, String name) {
+        assertThatThrownBy(() -> new User(id, password, name)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream illegalConstructorTest() {
+        return Stream.of(
+                Arguments.of(null, "123", "광"),
+                Arguments.of("", "123", "광"),
+                Arguments.of("nokchax", null, "광"),
+                Arguments.of("nokchax", "", "광"),
+                Arguments.of("nokchax", "123", null),
+                Arguments.of("nokchax", "123", "")
+        );
+    }
 }
