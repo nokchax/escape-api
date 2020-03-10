0c6b3f04943219edb07e93c0655bda96ffe730c6
nokchax <nokchax@gmail.com>
2020-03-10 21:09:35 +0900

    feat : make constructor and test

--- src/main/java/com/nokchax/escape/user/domain/User.java
+++ src/main/java/com/nokchax/escape/user/domain/User.java
@@ -3,8 +3,10 @@ package com.nokchax.escape.user.domain;
 import com.nokchax.escape.problem.domain.SolvedProblem;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.*;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -24,4 +26,21 @@ public class User {
     private int solvedProblemCount;
     @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private Set<SolvedProblem> solvedProblem = new HashSet<>();
+
+    public User(String id, String password, String name) {
+        checkInput(id, password, name);
+
+        this.id = id;
+        this.password = password;
+        this.name = name;
+    }
+
+    private void checkInput(String... inputs) {
+        Arrays.stream(inputs)
+                .forEach(input -> {
+                    if(StringUtils.isEmpty(input)) {
+                        throw new IllegalArgumentException("Input is null or empty");
+                    }
+                });
+    }
 }
--- src/test/java/com/nokchax/escape/problem/domain/SolvedProblemTest.java
+++ src/test/java/com/nokchax/escape/problem/domain/SolvedProblemTest.java
@@ -1,11 +1,16 @@
 package com.nokchax.escape.problem.domain;
 
 import com.nokchax.escape.problem.repository.ProblemRepository;
+import com.nokchax.escape.user.domain.User;
 import com.nokchax.escape.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @DataJpaTest
@@ -18,6 +23,16 @@ class SolvedProblemTest {
 
     @Test
     void testDDLQuery() {
+        User user = new User("nokchax", "123", "광");
 
+        assertThat(user.getSolvedProblemCount()).isEqualTo(0);
+        assertThat(user.getSolvedProblem()).isNotNull();
+        assertThat(user.getSolvedProblem().size()).isEqualTo(0);
     }
+
+//    @ParameterizedTest
+//    @MethodSource
+//    void testConstructorTest(String id, String password, String name) {
+//
+//    }
 }
