1f70e796b8194623843c758139183204900b72aa
nokchax <nokchax@gmail.com>
2020-03-15 17:59:59 +0900

    feat : get problem solve history with order by added query

--- src/main/java/com/nokchax/escape/problem/repository/ProblemSolveHistoryRepository.java
+++ src/main/java/com/nokchax/escape/problem/repository/ProblemSolveHistoryRepository.java
@@ -3,5 +3,8 @@ package com.nokchax.escape.problem.repository;
 import com.nokchax.escape.problem.domain.ProblemSolveHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProblemSolveHistoryRepository extends JpaRepository<ProblemSolveHistory, String> {
+    List<ProblemSolveHistory> findAllByOrderByTotalCountDescHardCountDescMediumCountDescEasyCountDesc();
 }
