d86e85e303dc3fd695b474b33640595b17ddff02
nokchax <nokchax@gmail.com>
2020-03-16 22:34:54 +0900

    feat : get problem solve histories using query annotation

--- src/main/java/com/nokchax/escape/problem/repository/ProblemSolveHistoryRepository.java
+++ src/main/java/com/nokchax/escape/problem/repository/ProblemSolveHistoryRepository.java
@@ -2,9 +2,13 @@ package com.nokchax.escape.problem.repository;
 
 import com.nokchax.escape.problem.domain.ProblemSolveHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProblemSolveHistoryRepository extends JpaRepository<ProblemSolveHistory, String> {
     List<ProblemSolveHistory> findAllByOrderByTotalCountDescHardCountDescMediumCountDescEasyCountDesc();
+
+    @Query(value = "SELECT solveHistory FROM ProblemSolveHistory solveHistory ORDER BY totalCount DESC, hardCount DESC, mediumCount DESC, easyCount DESC" )
+    List<ProblemSolveHistory> findAllOrdered();
 }
--- src/test/java/com/nokchax/escape/problem/domain/ProblemSolveHistoryTest.java
+++ src/test/java/com/nokchax/escape/problem/domain/ProblemSolveHistoryTest.java
@@ -106,4 +106,53 @@ class ProblemSolveHistoryTest {
         problemSolveHistoryRepository.findAllByOrderByTotalCountDescHardCountDescMediumCountDescEasyCountDesc();
         System.out.println("=========================================================After find all");
     }
+
+    /**
+     * using @Query annotation
+     * =========================================================Before find all
+     * Hibernate:
+     *     select
+     *         problemsol0_.user_id as user_id1_0_,
+     *         problemsol0_.easy_count as easy_cou2_0_,
+     *         problemsol0_.hard_count as hard_cou3_0_,
+     *         problemsol0_.medium_count as medium_c4_0_,
+     *         problemsol0_.total_count as total_co5_0_
+     *     from
+     *         ( SELECT
+     *             user_id,
+     *             COUNT(DISTINCT question_id) AS total_count,
+     *             COUNT(DISTINCT CASE
+     *                 WHEN difficulty = 'HARD' THEN question_id
+     *                 ELSE NULL
+     *             END) AS hard_count,
+     *             COUNT(DISTINCT CASE
+     *                 WHEN difficulty = 'MEDIUM' THEN question_id
+     *                 ELSE NULL
+     *             END) AS medium_count,
+     *             COUNT(DISTINCT CASE
+     *                 WHEN difficulty = 'EASY' THEN question_id
+     *                 ELSE NULL
+     *             END) AS easy_count
+     *         FROM
+     *             user_problem
+     *         LEFT JOIN
+     *             problem
+     *                 ON (
+     *                     user_problem.problem_id = problem.question_id
+     *                 )
+     *         GROUP BY
+     *             user_id ) problemsol0_
+     *     order by
+     *         problemsol0_.total_count DESC,
+     *         problemsol0_.hard_count DESC,
+     *         problemsol0_.medium_count DESC,
+     *         problemsol0_.easy_count DESC
+     * =========================================================After find all
+     */
+    @Test
+    void findAllAndOrderedQueryTest() {
+        System.out.println("=========================================================Before find all");
+        problemSolveHistoryRepository.findAllOrdered();
+        System.out.println("=========================================================After find all");
+    }
 }
