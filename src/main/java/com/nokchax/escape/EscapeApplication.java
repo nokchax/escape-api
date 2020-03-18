5ef419dfc4d115575f74e8e976f3ea0857f40a18
nokchax <nokchax@gmail.com>
2020-03-18 21:20:39 +0900

    comment : memo table structure

--- src/main/java/com/nokchax/escape/EscapeApplication.java
+++ src/main/java/com/nokchax/escape/EscapeApplication.java
@@ -18,4 +18,48 @@ public class EscapeApplication {
     }
 
     // TODO: 2020-03-18 DB 테이블 정리할겸 한번 그려보기 -> 개선사항 찾기
+    /**
+     * table list
+     *
+     * users
+     *  id
+     *  name
+     *  password
+     *  solve_question_count
+     *
+     * problem
+     *  question_id
+     *  difficulty
+     *  hide
+     *  title
+     *  title_slug
+     *  front_end_question_id
+     *
+     * user history
+     *  date_time
+     *  user_id
+     *  description
+     *  point
+     *
+     * user problem
+     *  problem_id
+     *  user_id
+     *  solved_time
+     *
+     * task
+     *  id
+     *  duration_type
+     *  end_date_time
+     *  goal_score
+     *  start_date_time
+     *
+     * task participants
+     *  task_id
+     *  user_id
+     *  easy
+     *  hard
+     *  medium
+     *  score
+     *
+     */
 }
