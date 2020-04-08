bd7f3a0446b190d9782f3fd5d8e0b2961d1213d7
nokchax <nokchax@gmail.com>
2020-04-08 16:48:55 +0900

    test : point repository layer test... complete

--- src/test/java/com/nokchax/escape/point/repository/PointRepositoryTest.java
+++ src/test/java/com/nokchax/escape/point/repository/PointRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.nokchax.escape.point.repository;
 
 import com.nokchax.escape.JpaTest;
+import com.nokchax.escape.point.domain.Point;
 import com.nokchax.escape.point.dto.PointDto;
+import com.nokchax.escape.user.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,21 +18,33 @@ class PointRepositoryTest extends JpaTest {
     private PointRepository pointRepository;
 
     @Test
-    void getPointSumTest() {
-        pointRepository.findAllPenaltyUsers();
-        System.out.println("=========================================================Before query");
-        List<PointDto> pointSummary = pointRepository.findAllUserPoint();
-        System.out.println("=========================================================After query");
-
-        pointSummary.forEach(System.out::println);
-    }
-
-    @Test
     void findAllUsersPointInfoTest() {
         List<PointDto> usersPoint = pointRepository.findAllUserPoint();
 
         assertThat(usersPoint).isNotNull();
         assertThat(usersPoint.size()).isEqualTo(15);
         assertThat(usersPoint.get(0).getPoint()).isGreaterThan(usersPoint.get(usersPoint.size() - 1).getPoint());
+
+        usersPoint.forEach(System.out::println);
+    }
+
+    @Test
+    void findPenaltyUsersPointInfoTest() {
+        List<PointDto> penaltyUsers = pointRepository.findAllPenaltyUsers();
+
+        assertThat(penaltyUsers).isNotNull();
+        assertThat(penaltyUsers.size()).isZero();
+    }
+
+    @Test
+    void findPenaltyUsersPointInfoTest_Exist() {
+        pointRepository.save(new Point(new User("nokchax1"), -100));
+
+        List<PointDto> penaltyUsers = pointRepository.findAllPenaltyUsers();
+
+        assertThat(penaltyUsers).isNotNull();
+        assertThat(penaltyUsers.size()).isNotZero();
+
+        penaltyUsers.forEach(System.out::println);
     }
 }
