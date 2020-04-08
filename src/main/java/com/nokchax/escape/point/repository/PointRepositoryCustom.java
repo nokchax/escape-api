42db0577b4dbbf2aa332d9d43e32f890ffc897a2
nokchax <nokchax@gmail.com>
2020-04-08 16:29:50 +0900

    test : point repository layer test... developing

--- src/main/java/com/nokchax/escape/point/repository/PointRepositoryCustom.java
+++ src/main/java/com/nokchax/escape/point/repository/PointRepositoryCustom.java
@@ -5,6 +5,9 @@ import com.nokchax.escape.point.dto.PointDto;
 import java.util.List;
 
 public interface PointRepositoryCustom {
+    /** 모든 유저의 포인트 정보를 불러옵니다. 포인트 내림차순으로 정렬하여 리턴 */
     List<PointDto> findAllUserPoint();
+
+    /** 포인트가 0보작 작은 유저의 포인트 정보만 불러옵니다 */
     List<PointDto> findAllPenaltyUsers();
 }
--- src/test/java/com/nokchax/escape/point/repository/PointRepositoryTest.java
+++ src/test/java/com/nokchax/escape/point/repository/PointRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.nokchax.escape.point.repository;
 
+import com.nokchax.escape.JpaTest;
 import com.nokchax.escape.point.dto.PointDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,18 +9,28 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
-@DataJpaTest
-@ActiveProfiles("dev")
-class PointRepositoryTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PointRepositoryTest extends JpaTest {
     @Autowired
     private PointRepository pointRepository;
 
     @Test
     void getPointSumTest() {
+        pointRepository.findAllPenaltyUsers();
         System.out.println("=========================================================Before query");
         List<PointDto> pointSummary = pointRepository.findAllUserPoint();
         System.out.println("=========================================================After query");
 
         pointSummary.forEach(System.out::println);
+    }
+
+    @Test
+    void findAllUsersPointInfoTest() {
+        List<PointDto> usersPoint = pointRepository.findAllUserPoint();
+
+        assertThat(usersPoint).isNotNull();
+        assertThat(usersPoint.size()).isEqualTo(15);
+        assertThat(usersPoint.get(0).getPoint()).isGreaterThan(usersPoint.get(usersPoint.size() - 1).getPoint());
     }
 }
