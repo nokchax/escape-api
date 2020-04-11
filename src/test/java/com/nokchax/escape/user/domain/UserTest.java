b1c033fe8e2c80de8cb5b0000ca28336acebb635
nokchax <nokchax@gmail.com>
2020-04-11 14:04:27 +0900

    feat : add display name and remove useless testcode

--- src/test/java/com/nokchax/escape/user/domain/UserTest.java
+++ src/test/java/com/nokchax/escape/user/domain/UserTest.java
@@ -1,25 +1,19 @@
 package com.nokchax.escape.user.domain;
 
 import com.nokchax.escape.ServiceLayerTest;
-import com.nokchax.escape.exception.UserNotFoundException;
 import com.nokchax.escape.mission.domain.Mission;
 import com.nokchax.escape.problem.domain.Problem;
 import com.nokchax.escape.problem.domain.SolvedProblem;
 import com.nokchax.escape.problem.repository.ProblemRepository;
 import com.nokchax.escape.user.repository.UserRepository;
-import com.zum.escape.api.users.dto.SolvedProblemDto;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Slf4j
@@ -32,17 +26,11 @@ class UserTest extends ServiceLayerTest {
     private ProblemRepository problemRepository;
 
     @Test
-    void findAllUsersTest() {
-        List<User> users = userRepository.findAll();
-
-        users.forEach(System.out::println);
-    }
-
-    @Test
     @Transactional
+    @DisplayName("회원이 새문제를 풀었을때 새로푼 문제들이 저장 되는지")
     void persistenceTest() {
         log.info("Before find");
-        User user = userRepository.findById("nokchax1").orElseThrow(IllegalArgumentException::new);
+        User user = userRepository.findByUserIdWithSolvedProblems("nokchax1").get(0);
         log.info("After find");
         Problem problem = problemRepository.findById(123L).orElseThrow(IllegalAccessError::new);
 
