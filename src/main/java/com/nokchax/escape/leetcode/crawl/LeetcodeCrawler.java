16fd72a051699bdf0738c45e514e2345d2f009e6
nokchax <nokchax@gmail.com>
2020-04-15 21:13:52 +0900

    remove : remove comment

--- src/main/java/com/nokchax/escape/leetcode/crawl/LeetcodeCrawler.java
+++ src/main/java/com/nokchax/escape/leetcode/crawl/LeetcodeCrawler.java
@@ -4,11 +4,6 @@ import com.nokchax.escape.leetcode.crawl.page.response.CrawledUserInfo;
 
 import java.util.Optional;
 
-/*
-    basic - crawl without login
-    selenium - crawl with login
-    json - crawl with json file
- */
 public interface LeetcodeCrawler<T> {
     CrawledUserInfo crawlUserInfo(T paramType);
 }
