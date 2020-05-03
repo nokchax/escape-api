2e3db37c0a8d7d70329e19d5976896997f0ca1ad
nokchax <nokchax@gmail.com>
2020-05-04 01:26:39 +0900

    feat : create new mission command

---
+++ src/main/java/com/nokchax/escape/command/commands/NewMissionCommand.java
@@ -0,0 +1,22 @@
+package com.nokchax.escape.command.commands;
+
+import com.nokchax.escape.command.Command;
+import com.nokchax.escape.command.CommandMapping;
+import com.nokchax.escape.command.SudoCommand;
+import com.nokchax.escape.mission.service.MissionService;
+import org.springframework.context.ApplicationContext;
+import org.telegram.telegrambots.meta.api.objects.Message;
+
+@CommandMapping(commands = {"newMission", "newmission", "nm"})
+public class NewMissionCommand extends Command<MissionService> implements SudoCommand {
+    public NewMissionCommand(Message message, ApplicationContext applicationContext) {
+        super(message, applicationContext);
+    }
+
+    @Override
+    protected String internalProcess() {
+        processor().createMission();
+
+        return "create complete";
+    }
+}
