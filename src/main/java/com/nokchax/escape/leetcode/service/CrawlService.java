42e0b863fcb1e536ca715c457977f55786563ffc
nokchax <nokchax@gmail.com>
2020-05-12 23:14:55 +0900

    refactor : remove unused field

--- src/main/java/com/nokchax/escape/leetcode/service/CrawlService.java
+++ src/main/java/com/nokchax/escape/leetcode/service/CrawlService.java
@@ -1,13 +1,10 @@
 package com.nokchax.escape.leetcode.service;
 
 import com.nokchax.escape.command.commands.UpdateCommand;
-import com.nokchax.escape.entry.service.EntryService;
 import com.nokchax.escape.leetcode.crawl.LeetcodeCrawler;
 import com.nokchax.escape.leetcode.crawl.api.LeetcodeApiCrawlerWithLogin;
 import com.nokchax.escape.leetcode.crawl.page.LeetcodePageCrawler;
-import com.nokchax.escape.problem.service.ProblemService;
 import com.nokchax.escape.user.domain.User;
-import com.nokchax.escape.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,9 +17,6 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CrawlService {
-    private final UserService userService;
-    private final EntryService entryService;
-    private final ProblemService problemService;
     private final LeetcodePageCrawler pageCrawler;
     private final LeetcodeApiCrawlerWithLogin apiCrawler;
     private List<LeetcodeCrawler<User>> crawlers;
