a44ec4df1f9c424543a0e772f5462669120433da
nokchax <nokchax@gmail.com>
2020-05-14 23:24:58 +0900

    feat : update class interface to annotation

--- src/main/java/com/nokchax/escape/command/Help.java
+++ src/main/java/com/nokchax/escape/command/Help.java
@@ -1,4 +1,11 @@
 package com.nokchax.escape.command;
 
-public interface Help {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Help {
 }
