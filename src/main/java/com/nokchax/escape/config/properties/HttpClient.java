320cc5cbac34ff8eafc4a0852c5a44ad726a03dc
nokchax <nokchax@gmail.com>
2020-06-01 20:41:49 +0900

    feat: add http client configuration properties class

--- src/main/java/com/nokchax/escape/config/properties/HttpClient.java
+++ src/main/java/com/nokchax/escape/config/properties/HttpClient.java
@@ -1,4 +1,8 @@
 package com.nokchax.escape.config.properties;
 
 public class HttpClient {
+    private int maxConnTotal;
+    private int maxConnPerRoute;
+    private int connectTimeout;//mill
+    private int readTimeout;//mill
 }
