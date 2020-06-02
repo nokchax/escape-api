502758d84b0fb02359ae7de9122c4540e36b9651
nokchax <nokchax@gmail.com>
2020-06-02 23:32:57 +0900

    feat: add annotations

--- src/main/java/com/nokchax/escape/config/properties/HttpClient.java
+++ src/main/java/com/nokchax/escape/config/properties/HttpClient.java
@@ -1,5 +1,10 @@
 package com.nokchax.escape.config.properties;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "httpclient")
 public class HttpClient {
     private int maxConnTotal;
     private int maxConnPerRoute;
