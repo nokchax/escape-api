2e3cb159c06fd45c91e5b577b0244b9555a24960
nokchax <nokchax@gmail.com>
2020-06-15 23:52:28 +0900

    chore: remove unused method

--- src/main/java/com/nokchax/escape/exception/CrawlException.java
+++ src/main/java/com/nokchax/escape/exception/CrawlException.java
@@ -2,10 +2,6 @@ package com.nokchax.escape.exception;
 
 public class CrawlException extends EscapeApiException {
 
-    public CrawlException() {
-        super("Fail to crawl");
-    }
-
     public CrawlException(String userId) {
         super("Fail to crawl : " + userId);
     }
