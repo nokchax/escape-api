fdf5274dbefa47ffbcb9769ccd90d337d4cc3200
nokchax <nokchax@gmail.com>
2020-06-17 23:46:37 +0900

    chore: add private constructor

--- src/main/java/com/nokchax/escape/leetcode/crawl/Leetcode.java
+++ src/main/java/com/nokchax/escape/leetcode/crawl/Leetcode.java
@@ -1,6 +1,8 @@
 package com.nokchax.escape.leetcode.crawl;
 
 public class Leetcode {
+    private Leetcode() {}
+
     public static final String LEETCODE_URL = "https://leetcode.com/";
     public static final String LEETCODE_API_URL = LEETCODE_URL + "api/problems/all/";
     public static final String LEETCODE_LOGIN_PAGE = LEETCODE_URL + "accounts/login/";
